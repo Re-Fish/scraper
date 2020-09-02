d4cf949b6cc6dad7a541abb9c0623410364a8be0
Roman Klyucharev <rk@funpart.io>
2020-09-02 22:04:07 +0500

    output to html

--- project.js
+++ project.js
@@ -4,6 +4,7 @@ const jquery = require('jquery');
 const countBy = require('lodash/countBy');
 const sortBy = require('lodash/sortBy');
 const reverse = require('lodash/reverse');
+const open = require('open');
 
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
@@ -47,24 +48,48 @@ const getStats = (allStrings) => {
     // count unique
     const counted = countBy(allStrings);
     // make array
-    const countedArray = Object.keys(counted).map((text) => ({
-        text,
-        count: counted[text],
+    const countedArray = Object.keys(counted).map((value) => ({
+        value,
+        count: counted[value],
     }));
     // sort
     return reverse(sortBy(countedArray, 'count'));
 };
 
-fs.readFile('./innoscripta GmbH.html', 'utf8', (err, data) => {
+const generateHtmlOutput = (result) => {
+    let html = '<html><body>';
+    Object.keys(result).forEach((key) => {
+        html += `<h2>${key}</h2>`;
+        html += '<table style="width:100%;max-width:800px">';
+        html += '<tr><th style="width:80%">Value</th><th>Count</th></tr>';
+        if (result[key].length) {
+            result[key].forEach(({ value, count }) => {
+                html += `<tr><td>${value}</td><td>${count}</td></tr>`;
+            });
+        } else {
+            html += `<tr><td colspan="2" style="color:lightcoral"><i>No data found</i></td></tr>`;
+        }
+        html += '</table>';
+    });
+    html += '</table></body></html>';
+    return html;
+};
+
+fs.readFile('./innoscripta GmbH.htm', 'utf8', (err, data) => {
     const page = $(data);
 
     const words = extractWords(page);
     const otherSiteLinks = extractLinks(page);
     const pictures = extractPictures(page);
 
-    console.log({
-        words: getStats(words),
-        links: getStats(otherSiteLinks),
-        images: getStats(pictures)
+    const results = {
+        Words: getStats(words),
+        Links: getStats(otherSiteLinks),
+        Pictures: getStats(pictures)
+    };
+    const htmlOutput = generateHtmlOutput(results);
+    fs.writeFile('output.html', htmlOutput, () => {
+        console.log('Results are here in output.html! It will be opened in default browser...');
+        return open('./output.html');
     });
 });
